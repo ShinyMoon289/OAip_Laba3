78518ffb3958c7e0f73f3373fdfbcd12052ceff9
Илья <76493639+ShinyMoon289@users.noreply.github.com>
2022-10-12 20:24:59 +0300

    Add files via upload

---
+++ No.2(variant10)/No.2(variant10).cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+int main()
+{
+    int i;
+    bool k = false;
+    for(i=10; i<100; i++)
+    {
+        if ((i / 10) + (i % 10) == 11)
+        {
+            if (i + 27 == (i % 10) * 10 + (i / 10))
+            {
+                k = true;
+                printf("Найдено число %d", i);
+            }
+        }
+    }
+    if (k == false) printf("Таких чисел нет.");
+
+    return 0;
+}
+
---
+++ ZadanieNa9(5)/ZadanieNa9(5).cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include<math.h>
+
+int main()
+{
+	double pi=1;
+	int i;
+	
+	for (i = 1; i < 501; i++)
+	{
+		pi = pi + ((1./ (2*i+1))*pow(-1,i));
+	}
+	pi *= 4;
+	printf("%f", pi);
+	return 0;
+}
